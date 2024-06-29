152c0afd09f19de64c952e0db3f2f45a2bf7b653
Sector <35609551+XSectoRX@users.noreply.github.com>
2024-06-29 12:59:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# socialNetwork
+ Делаем и Деплоим Фуллстак Приложение на React, TypeScript, Redux Toolkit, Express _ 11  часов
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const UserController = require("../controllers");
+const { authenticateToken } = require("../middleware/auth");
+const uploadDestination = 'uploads'
+
+//Показываем, где хранить файлы
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req,res,cb){
+    cb(null, file.originalname);
+  }
+})
+
+const uploads = multer({storage:storage})
+
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.get('/current', authenticateToken, UserController.current)
+router.get('/users/:id', authenticateToken, UserController.getUserById)
+router.put('/users/:id', authenticateToken, UserController.updateUser)
+
+module.exports = router;
