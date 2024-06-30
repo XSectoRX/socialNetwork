11fec4b84d83fe7921fb190fd54fb8dfef53bfcc
Sector <35609551+XSectoRX@users.noreply.github.com>
2024-06-30 18:47:17 +0300

    controller
      UserController,
      PostController,
      FollowController,
      LikeController,
      CommentController

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,13 @@
-const UserController = require('./user-controller')
+const UserController = require('./user-controller');
+const PostController = require('./post-controller');
+const FollowController = require('./follow-controller');
+const LikeController = require('./like-controller');
+const CommentController = require('./comment-controller');
 
-module.exports = UserController;
+module.exports = {
+  UserController,
+  PostController,
+  FollowController,
+  LikeController,
+  CommentController
+};
