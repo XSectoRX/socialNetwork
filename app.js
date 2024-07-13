2db42d4ef4f24b3b6db537021b3b5670f107fd63
Sector <35609551+XSectoRX@users.noreply.github.com>
2024-07-13 22:00:57 +0300

    build PROM
    PROM

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fs = require("fs");
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var path = require('path');
+var fs = require('fs');
+require('dotenv').config();
 
-const app = express();
+var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.set('view engine', 'jade');
-//Раздавать статический файлы из папки 'upLoads'
-app.use('/uploads', express.static('/uploads'))
+app.set('view engine', 'pug');
+// Раздавать статические файлы из папки 'uploads'
+app.use('/uploads', express.static('uploads'));
 
-app.use('/api', require('./routes'))
+app.use('/api', require('./routes'));
 
-if(!fs.existsSync('uploads')){
-  fs.mkdirSync('uploads')
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
 }
 
 // catch 404 and forward to error handler
